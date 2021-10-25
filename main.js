35155032dddfe09dbbca0b6f757625913c7b0dcf
Gal Podlipnik <itzgal2004@gmail.com>
2021-10-25 19:23:04 +0200

    webpage change

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ audio.loop = true;
 audio.onended = function () {
     audio.play();
 }
+
+function audio() {
+    var audio = new Audio("opsie.mp3");
+
+    audio.oncanplaythrough = function () {
+        audio.play();
+    }
+
+    audio.loop = true;
+
+    audio.onended = function () {
+        audio.play();
+    }
+}
